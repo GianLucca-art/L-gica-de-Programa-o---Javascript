6a9943a644cfdaf79a4f1092fc61c28977036caf
giancastro <gianlucca.galldorr@gmail.com>
2026-03-29 06:02:01 -0300

    adicionar exercicios de lógica de programação

---
+++ desafios.js
@@ -0,0 +1,7 @@
+console.log("Digite o nome do jogador");
+
+let nickname = "Gian o grão mestre dos pokémons"
+const notificacao = "pokemon GO diz: " 
+console.log(notificacao);
+console.log(nickname  + " entrou no servidor")
+console.log(notificacao + " voce foi atacado por um pokémon selvagem")
---
+++ desafios2.js
@@ -0,0 +1,8 @@
+let poteCafe = "café pilão"
+let poteBiscoito = "biscoito recheado"
+let acucar = "açucar cristal"
+
+const mensagemDaVovo = "Na cozinda da vovó hoje tem: "
+console.log(mensagemDaVovo + poteCafe + ", " + poteBiscoito + " e " + acucar);
+acucar = "açucar refinado"
+console.log(mensagemDaVovo + poteCafe + ", " + poteBiscoito + " e " + acucar);
